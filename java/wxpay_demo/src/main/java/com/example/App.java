8764b20049298fea899795ef89f886f07fd8c326
shuoweiwu <shuoweiwu@tencent.com>
2025-01-14 18:57:15 +0800

    init Project

---
+++ java/wxpay_demo/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello World!");
+    }
+}
